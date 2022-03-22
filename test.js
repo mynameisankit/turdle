bb4c7538b662f2dbd7cc89af3462f0a8b2450308
Ankit Kumar <ankit.kumar19@iiitg.ac.in>
2022-03-22 22:39:41 +0530

    Refactor

--- test.js
+++
@@ -1,7 +0,0 @@
-const answersDictionary = require('./database/answers.json');
-
-const diffTime = Math.abs(new Date() - new Date('2021/6/19'));
-const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) - 1;
-let solution = answersDictionary[diffDays%(answersDictionary.length)];
-
-console.log(solution);
