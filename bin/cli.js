185a91e42f69163a0f97e347de7b9f48c1fe9b76
Ankit Kumar <ankit.kumar19@iiitg.ac.in>
2022-03-22 22:48:06 +0530

    Refactor

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+
 'use strict';
+
 const React = require('react');
 const importJsx = require('import-jsx');
 const { render } = require('ink');
@@ -10,13 +12,6 @@ const ui = importJsx('../lib/ui');
 const cli = meow(`
 	Usage
 	  $ turdle
-
-	Options
-		--name  Your name
-
-	Examples
-	  $ turdle --name=Jane
-	  Hello, Jane
 `);
 
 render(React.createElement(ui, cli.flags));
